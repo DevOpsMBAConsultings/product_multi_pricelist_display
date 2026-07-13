ae060c6c2be1b1e9ff9ec7c010bec8deba7f3499
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-12 21:11:48 -0500

    Fix: replace _setup_fields registration with runtime get_view and read overrides for both product.template and product.product

--- models/product.py
+++ models/product.py
@@ -1,119 +1,123 @@
 from odoo import models, fields, api
 from lxml import etree
 
+
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    def _setup_fields(self):
-        cls = type(self)
-        try:
-            self.env.cr.execute("SELECT 1 FROM pg_catalog.pg_class WHERE relname = 'product_pricelist'")
-            if self.env.cr.fetchone():
-                self.env.cr.execute("SELECT id, name FROM product_pricelist WHERE display_in_product_list = true")
-                pricelists = self.env.cr.fetchall()
-                for pricelist_id, pricelist_name in pricelists:
-                    field_name = f"price_pricelist_{pricelist_id}"
-                    if field_name not in cls._fields:
-                        field = fields.Float(
-                            string=f"Price ({pricelist_name})",
-                            compute="_compute_pricelist_prices",
-                            store=False,
-                            readonly=True,
-                        )
-                        cls._add_field(field_name, field)
-        except Exception:
-            pass
-        super()._setup_fields()
-
-    @api.depends("list_price")
-    def _compute_pricelist_prices(self):
-        """Compute prices from pricelists marked to display"""
-        pricelists = self.env["product.pricelist"].search(
-            [("display_in_product_list", "=", True)]
-        )
-        for template in self:
-            product = template.product_variant_id
-            for pricelist in pricelists:
-                field_name = f"price_pricelist_{pricelist.id}"
-                if product:
-                    try:
-                        price = pricelist._get_product_price(product, quantity=1.0)
-                        template[field_name] = price
-                    except Exception:
-                        template[field_name] = 0.0
-                else:
-                    template[field_name] = 0.0
-
     @api.model
-    def _get_view(self, view_id=None, view_type='form', **options):
-        arch, view = super()._get_view(view_id=view_id, view_type=view_type, **options)
-        if view_type in ('tree', 'list'):
-            target = arch.xpath("//field[@name='list_price']") or arch.xpath("//field[@name='lst_price']") or arch.xpath("//field[@name='name']")
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super().get_view(view_id=view_id, view_type=view_type, **options)
+        if view_type == 'list':
+            arch_el = etree.fromstring(res['arch'])
+            target = arch_el.xpath("//field[@name='list_price']") or arch_el.xpath("//field[@name='lst_price']") or arch_el.xpath("//field[@name='name']")
             if target:
                 pricelists = self.env["product.pricelist"].search(
                     [("display_in_product_list", "=", True)]
                 )
                 for pricelist in pricelists:
                     field_name = f"price_pricelist_{pricelist.id}"
-                    if not arch.xpath(f"//field[@name='{field_name}']"):
+                    if not arch_el.xpath(f"//field[@name='{field_name}']"):
                         new_field = etree.Element('field', name=field_name, optional="show")
                         target[0].addnext(new_field)
-        return arch, view
+                        
+                        # Add field definition to the view metadata
+                        if 'fields' in res and field_name not in res['fields']:
+                            res['fields'][field_name] = {
+                                'type': 'float',
+                                'string': f"Price ({pricelist.name})",
+                                'readonly': True,
+                                'sortable': False,
+                                'searchable': False,
+                                'store': False,
+                            }
+                res['arch'] = etree.tostring(arch_el, encoding='unicode')
+        return res
+
+    def read(self, fields=None, load='_classic_read'):
+        if fields:
+            pricelist_fields = [f for f in fields if f.startswith('price_pricelist_')]
+            standard_fields = [f for f in fields if not f.startswith('price_pricelist_')]
+            
+            res = super().read(standard_fields, load=load)
+            
+            if pricelist_fields:
+                pricelist_ids = [int(f.replace('price_pricelist_', '')) for f in pricelist_fields]
+                pricelists = self.env['product.pricelist'].browse(pricelist_ids)
+                pricelist_map = {p.id: p for p in pricelists}
+                
+                for record_dict in res:
+                    record = self.browse(record_dict['id'])
+                    product = record.product_variant_id
+                    for f in pricelist_fields:
+                        p_id = int(f.replace('price_pricelist_', ''))
+                        pricelist = pricelist_map.get(p_id)
+                        if pricelist and product:
+                            try:
+                                record_dict[f] = pricelist._get_product_price(product, quantity=1.0)
+                            except Exception:
+                                record_dict[f] = 0.0
+                        else:
+                            record_dict[f] = 0.0
+            return res
+        return super().read(fields, load=load)
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    def _setup_fields(self):
-        cls = type(self)
-        try:
-            self.env.cr.execute("SELECT 1 FROM pg_catalog.pg_class WHERE relname = 'product_pricelist'")
-            if self.env.cr.fetchone():
-                self.env.cr.execute("SELECT id, name FROM product_pricelist WHERE display_in_product_list = true")
-                pricelists = self.env.cr.fetchall()
-                for pricelist_id, pricelist_name in pricelists:
-                    field_name = f"price_pricelist_{pricelist_id}"
-                    if field_name not in cls._fields:
-                        field = fields.Float(
-                            string=f"Price ({pricelist_name})",
-                            compute="_compute_pricelist_prices",
-                            store=False,
-                            readonly=True,
-                        )
-                        cls._add_field(field_name, field)
-        except Exception:
-            pass
-        super()._setup_fields()
-
-    @api.depends("list_price")
-    def _compute_pricelist_prices(self):
-        """Compute prices from pricelists marked to display"""
-        pricelists = self.env["product.pricelist"].search(
-            [("display_in_product_list", "=", True)]
-        )
-        for product in self:
-            for pricelist in pricelists:
-                field_name = f"price_pricelist_{pricelist.id}"
-                try:
-                    price = pricelist._get_product_price(product, quantity=1.0)
-                    product[field_name] = price
-                except Exception:
-                    product[field_name] = 0.0
-
     @api.model
-    def _get_view(self, view_id=None, view_type='form', **options):
-        arch, view = super()._get_view(view_id=view_id, view_type=view_type, **options)
-        if view_type in ('tree', 'list'):
-            target = arch.xpath("//field[@name='list_price']") or arch.xpath("//field[@name='lst_price']") or arch.xpath("//field[@name='name']")
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super().get_view(view_id=view_id, view_type=view_type, **options)
+        if view_type == 'list':
+            arch_el = etree.fromstring(res['arch'])
+            target = arch_el.xpath("//field[@name='list_price']") or arch_el.xpath("//field[@name='lst_price']") or arch_el.xpath("//field[@name='name']")
             if target:
                 pricelists = self.env["product.pricelist"].search(
                     [("display_in_product_list", "=", True)]
                 )
                 for pricelist in pricelists:
                     field_name = f"price_pricelist_{pricelist.id}"
-                    if not arch.xpath(f"//field[@name='{field_name}']"):
+                    if not arch_el.xpath(f"//field[@name='{field_name}']"):
                         new_field = etree.Element('field', name=field_name, optional="show")
                         target[0].addnext(new_field)
-        return arch, view
+                        
+                        # Add field definition to the view metadata
+                        if 'fields' in res and field_name not in res['fields']:
+                            res['fields'][field_name] = {
+                                'type': 'float',
+                                'string': f"Price ({pricelist.name})",
+                                'readonly': True,
+                                'sortable': False,
+                                'searchable': False,
+                                'store': False,
+                            }
+                res['arch'] = etree.tostring(arch_el, encoding='unicode')
+        return res
 
-
+    def read(self, fields=None, load='_classic_read'):
+        if fields:
+            pricelist_fields = [f for f in fields if f.startswith('price_pricelist_')]
+            standard_fields = [f for f in fields if not f.startswith('price_pricelist_')]
+            
+            res = super().read(standard_fields, load=load)
+            
+            if pricelist_fields:
+                pricelist_ids = [int(f.replace('price_pricelist_', '')) for f in pricelist_fields]
+                pricelists = self.env['product.pricelist'].browse(pricelist_ids)
+                pricelist_map = {p.id: p for p in pricelists}
+                
+                for record_dict in res:
+                    product = self.browse(record_dict['id'])
+                    for f in pricelist_fields:
+                        p_id = int(f.replace('price_pricelist_', ''))
+                        pricelist = pricelist_map.get(p_id)
+                        if pricelist and product:
+                            try:
+                                record_dict[f] = pricelist._get_product_price(product, quantity=1.0)
+                            except Exception:
+                                record_dict[f] = 0.0
+                        else:
+                            record_dict[f] = 0.0
+            return res
+        return super().read(fields, load=load)
